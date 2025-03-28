92b0a02d2734878286dc5920275443d919001a71
Slide <649518322@qq.com>
2025-03-28 16:19:00 +0800

    feat: add fuzz test for workloadspread and uniteddeployment (#1713)
    Signed-off-by: yechun <yechun.yc@alibaba-inc.com>

---
+++ pkg/controller/uniteddeployment/fuzz_uniteddeployment_controller_test.go
@@ -0,0 +1,309 @@
+/*
+Copyright 2021 The Kruise Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package uniteddeployment
+
+import (
+	"strconv"
+	"strings"
+	"testing"
+
+	fuzz "github.com/AdaLogics/go-fuzz-headers"
+	appsv1alpha1 "github.com/openkruise/kruise/apis/apps/v1alpha1"
+	"github.com/openkruise/kruise/pkg/controller/uniteddeployment/adapter"
+	fuzzutils "github.com/openkruise/kruise/pkg/util/fuzz"
+	v1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+)
+
+var fakeScheme = runtime.NewScheme()
+
+func init() {
+	_ = appsv1alpha1.AddToScheme(fakeScheme)
+	_ = clientgoscheme.AddToScheme(fakeScheme)
+}
+
+func FuzzParseSubsetReplicas(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		cf := fuzz.NewConsumer(data)
+
+		udReplicasInt, err := cf.GetInt()
+		if err != nil {
+			return
+		}
+		udReplicas := int32(udReplicasInt)
+
+		subsetReplicas, err := fuzzutils.GenerateSubsetReplicas(cf)
+		if err != nil {
+			return
+		}
+
+		replicas, err := ParseSubsetReplicas(udReplicas, subsetReplicas)
+		if err != nil {
+			return
+		}
+
+		switch subsetReplicas.Type {
+		case intstr.Int:
+			expected := subsetReplicas.IntVal
+			if replicas != expected {
+				t.Errorf("Mismatch for Int type: expected %d, got %d", expected, replicas)
+			}
+		case intstr.String:
+			strVal := subsetReplicas.StrVal
+			if !strings.HasSuffix(strVal, "%") {
+				return
+			}
+			percentStr := strings.TrimSuffix(strVal, "%")
+			percent, errParse := strconv.Atoi(percentStr)
+			if errParse != nil {
+				t.Errorf("Failed to parse percentage value from %s: %v", strVal, errParse)
+				return
+			}
+			expected := round(float64(udReplicas) * float64(percent) / 100)
+			if int32(expected) != replicas {
+				t.Errorf("Mismatch for percentage %s%%: expected %d, got %d", percentStr, expected, replicas)
+			}
+		default:
+			t.Errorf("Unsupported type %v", subsetReplicas.Type)
+		}
+	})
+}
+
+func FuzzApplySubsetTemplate(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		cf := fuzz.NewConsumer(data)
+
+		ud := &appsv1alpha1.UnitedDeployment{}
+		if err := cf.GenerateStruct(ud); err != nil {
+			return
+		}
+
+		if err := fuzzutils.GenerateUnitedDeploymentSubset(cf, ud); err != nil {
+			return
+		}
+
+		var subAdapter adapter.Adapter
+		choice, err := cf.GetInt()
+		if err != nil {
+			return
+		}
+		switch choice % 4 {
+		case 0:
+			subAdapter = initCloneSet(cf, ud)
+		case 1:
+			subAdapter = initStatefulSet(cf, ud)
+		case 2:
+			subAdapter = initDeployment(cf, ud)
+		case 3:
+			subAdapter = initAdvancedStatefulSet(cf, ud)
+		}
+
+		revision, err := cf.GetString()
+		if err != nil {
+			return
+		}
+		replicas, err := cf.GetInt()
+		if err != nil {
+			return
+		}
+		partition, err := cf.GetInt()
+		if err != nil {
+			return
+		}
+
+		_ = subAdapter.ApplySubsetTemplate(
+			ud,
+			ud.Spec.Topology.Subsets[0].Name, // Use first subset
+			revision,
+			int32(replicas),
+			int32(partition),
+			subAdapter.NewResourceObject(),
+		)
+	})
+}
+
+func FuzzReplicaAllocator(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		cf := fuzz.NewConsumer(data)
+		ud := &appsv1alpha1.UnitedDeployment{}
+
+		if err := fuzzutils.GenerateUnitedDeploymentSubset(cf, ud); err != nil {
+			return
+		}
+
+		if err := fuzzutils.GenerateUnitedDeploymentReplicas(cf, ud); err != nil {
+			return
+		}
+
+		if err := fuzzutils.GenerateUnitedDeploymentScheduleStrategy(cf, ud); err != nil {
+			return
+		}
+
+		// Map each subset name to its corresponding Subset structure
+		nameToSubset := make(map[string]*Subset)
+		for _, subsetDef := range ud.Spec.Topology.Subsets {
+			name := subsetDef.Name
+			if name == "" {
+				continue
+			}
+
+			s := &Subset{
+				Spec: SubsetSpec{
+					SubsetName: name,
+				},
+			}
+
+			s.Spec.Replicas = 5
+			if rep, err := cf.GetInt(); err == nil {
+				s.Status.Replicas = int32(rep)
+			} else {
+				s.Status.Replicas = 5
+			}
+
+			if b, err := cf.GetBool(); err == nil {
+				s.Status.UnschedulableStatus.Unschedulable = b
+			}
+
+			s.Status.UnschedulableStatus.PendingPods = 0
+			nameToSubset[name] = s
+		}
+
+		_, _ = NewReplicaAllocator(ud).Alloc(&nameToSubset)
+	})
+}
+
+func handleTemplate[T any](
+	structured bool,
+	cf *fuzz.ConsumeFuzzer,
+	template **T,
+	newTemplate func() *T,
+	fillTemplate func(t *T, ud *appsv1alpha1.UnitedDeployment),
+	ud *appsv1alpha1.UnitedDeployment,
+) {
+	if structured {
+		if *template == nil {
+			*template = newTemplate()
+		}
+		fillTemplate(*template, ud)
+	} else {
+		temp := newTemplate()
+		if err := cf.GenerateStruct(temp); err == nil {
+			*template = temp
+		}
+	}
+}
+
+func initTemplateMetadata(cf *fuzz.ConsumeFuzzer, meta *metav1.ObjectMeta, ud *appsv1alpha1.UnitedDeployment) {
+	labels := make(map[string]string)
+	if err := cf.FuzzMap(&labels); err != nil {
+		return
+	}
+	annotations := make(map[string]string)
+	if err := cf.FuzzMap(&annotations); err != nil {
+		return
+	}
+	matchLabels := make(map[string]string)
+	if err := cf.FuzzMap(&matchLabels); err != nil {
+		return
+	}
+	meta.Labels = labels
+	meta.Annotations = annotations
+	ud.Spec.Selector.MatchLabels = matchLabels
+}
+
+func initCloneSet(cf *fuzz.ConsumeFuzzer, ud *appsv1alpha1.UnitedDeployment) adapter.Adapter {
+	structured, err := cf.GetBool()
+	if err != nil {
+		structured = false
+	}
+	handleTemplate[appsv1alpha1.CloneSetTemplateSpec](
+		structured,
+		cf,
+		&ud.Spec.Template.CloneSetTemplate,
+		func() *appsv1alpha1.CloneSetTemplateSpec { return &appsv1alpha1.CloneSetTemplateSpec{} },
+		func(t *appsv1alpha1.CloneSetTemplateSpec, ud *appsv1alpha1.UnitedDeployment) {
+			initTemplateMetadata(cf, &t.ObjectMeta, ud)
+		},
+		ud,
+	)
+	return &adapter.CloneSetAdapter{Scheme: fakeScheme}
+}
+
+func initDeployment(cf *fuzz.ConsumeFuzzer, ud *appsv1alpha1.UnitedDeployment) adapter.Adapter {
+	structured, err := cf.GetBool()
+	if err != nil {
+		structured = false
+	}
+	handleTemplate[appsv1alpha1.DeploymentTemplateSpec](
+		structured,
+		cf,
+		&ud.Spec.Template.DeploymentTemplate,
+		func() *appsv1alpha1.DeploymentTemplateSpec { return &appsv1alpha1.DeploymentTemplateSpec{} },
+		func(t *appsv1alpha1.DeploymentTemplateSpec, ud *appsv1alpha1.UnitedDeployment) {
+			initTemplateMetadata(cf, &t.ObjectMeta, ud)
+		},
+		ud,
+	)
+	return &adapter.DeploymentAdapter{Scheme: fakeScheme}
+}
+
+func initAdvancedStatefulSet(cf *fuzz.ConsumeFuzzer, ud *appsv1alpha1.UnitedDeployment) adapter.Adapter {
+	structured, err := cf.GetBool()
+	if err != nil {
+		structured = false
+	}
+	handleTemplate[appsv1alpha1.AdvancedStatefulSetTemplateSpec](
+		structured,
+		cf,
+		&ud.Spec.Template.AdvancedStatefulSetTemplate,
+		func() *appsv1alpha1.AdvancedStatefulSetTemplateSpec {
+			return &appsv1alpha1.AdvancedStatefulSetTemplateSpec{}
+		},
+		func(t *appsv1alpha1.AdvancedStatefulSetTemplateSpec, ud *appsv1alpha1.UnitedDeployment) {
+			if t.Spec.UpdateStrategy.Type == "" {
+				t.Spec.UpdateStrategy.Type = v1.RollingUpdateStatefulSetStrategyType
+			}
+			initTemplateMetadata(cf, &t.ObjectMeta, ud)
+		},
+		ud,
+	)
+	return &adapter.AdvancedStatefulSetAdapter{Scheme: fakeScheme}
+}
+
+func initStatefulSet(cf *fuzz.ConsumeFuzzer, ud *appsv1alpha1.UnitedDeployment) adapter.Adapter {
+	structured, err := cf.GetBool()
+	if err != nil {
+		structured = false
+	}
+	handleTemplate[appsv1alpha1.StatefulSetTemplateSpec](
+		structured,
+		cf,
+		&ud.Spec.Template.StatefulSetTemplate,
+		func() *appsv1alpha1.StatefulSetTemplateSpec { return &appsv1alpha1.StatefulSetTemplateSpec{} },
+		func(t *appsv1alpha1.StatefulSetTemplateSpec, ud *appsv1alpha1.UnitedDeployment) {
+			if t.Spec.UpdateStrategy.Type == "" {
+				t.Spec.UpdateStrategy.Type = v1.RollingUpdateStatefulSetStrategyType
+			}
+			initTemplateMetadata(cf, &t.ObjectMeta, ud)
+		},
+		ud,
+	)
+	return &adapter.StatefulSetAdapter{Scheme: fakeScheme}
+}
---
+++ pkg/controller/workloadspread/fuzz_workloadspread_controller_test.go
@@ -0,0 +1,189 @@
+/*
+Copyright 2021 The Kruise Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package workloadspread
+
+import (
+	"context"
+	"encoding/json"
+	"strconv"
+	"testing"
+
+	fuzz "github.com/AdaLogics/go-fuzz-headers"
+	appsv1alpha1 "github.com/openkruise/kruise/apis/apps/v1alpha1"
+	fuzzutils "github.com/openkruise/kruise/pkg/util/fuzz"
+	wsutil "github.com/openkruise/kruise/pkg/util/workloadspread"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+var (
+	fakeScheme = runtime.NewScheme()
+)
+
+func init() {
+	_ = clientgoscheme.AddToScheme(fakeScheme)
+	_ = appsv1alpha1.AddToScheme(fakeScheme)
+}
+
+func FuzzPatchFavoriteSubsetMetadataToPod(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		cf := fuzz.NewConsumer(data)
+
+		pod := &corev1.Pod{}
+		if err := cf.GenerateStruct(pod); err != nil {
+			return
+		}
+
+		// Cleanup deletion timestamp when no finalizers exist
+		if pod.GetDeletionTimestamp() != nil && len(pod.GetFinalizers()) == 0 {
+			pod.SetDeletionTimestamp(nil)
+		}
+
+		ws := &appsv1alpha1.WorkloadSpread{}
+		if err := cf.GenerateStruct(ws); err != nil {
+			return
+		}
+		if ws.GetAnnotations() == nil {
+			ws.Annotations = make(map[string]string)
+		}
+
+		ignore, err := cf.GetBool()
+		if err != nil {
+			return
+		}
+		ws.GetAnnotations()[IgnorePatchExistingPodsAnnotation] = strconv.FormatBool(ignore)
+
+		subset := &appsv1alpha1.WorkloadSpreadSubset{}
+		if err := cf.GenerateStruct(subset); err != nil {
+			return
+		}
+
+		if err := fuzzutils.GenerateWorkloadSpreadSubsetPatch(cf, subset); err != nil {
+			return
+		}
+
+		r := &ReconcileWorkloadSpread{
+			Client: fake.NewClientBuilder().
+				WithScheme(fakeScheme).
+				WithObjects(pod.DeepCopy()).
+				Build(),
+		}
+
+		if err := r.patchFavoriteSubsetMetadataToPod(pod, ws, subset); err != nil {
+			return
+		}
+
+		updatedPod := &corev1.Pod{}
+		if err := r.Client.Get(context.TODO(), client.ObjectKeyFromObject(pod), updatedPod); err != nil {
+			t.Errorf("Failed to fetch updated Pod: %v", err)
+			return
+		}
+
+		// Calculate expected annotations/labels inline
+		expectedAnnotations := make(map[string]string)
+		expectedLabels := make(map[string]string)
+
+		// Copy original pod's metadata
+		if originalAnnotations := pod.GetAnnotations(); originalAnnotations != nil {
+			for k, v := range originalAnnotations {
+				expectedAnnotations[k] = v
+			}
+		}
+		if originalLabels := pod.GetLabels(); originalLabels != nil {
+			for k, v := range originalLabels {
+				expectedLabels[k] = v
+			}
+		}
+
+		// Apply subset patch if applicable
+		if subset.Patch.Raw != nil && ws.Annotations[IgnorePatchExistingPodsAnnotation] != "true" {
+			var patchField map[string]interface{}
+			if err := json.Unmarshal(subset.Patch.Raw, &patchField); err == nil {
+				if metadata, ok := patchField["metadata"].(map[string]interface{}); ok {
+					if labels, ok := metadata["labels"].(map[string]interface{}); ok {
+						for k, v := range labels {
+							if vs, ok := v.(string); ok {
+								expectedLabels[k] = vs
+							}
+						}
+					}
+					if annotations, ok := metadata["annotations"].(map[string]interface{}); ok {
+						for k, v := range annotations {
+							if vs, ok := v.(string); ok {
+								expectedAnnotations[k] = vs
+							}
+						}
+					}
+				}
+			}
+		}
+
+		injectWS := &wsutil.InjectWorkloadSpread{
+			Name:   ws.Name,
+			Subset: subset.Name,
+		}
+		injectWSBytes, _ := json.Marshal(injectWS)
+		expectedAnnotations[wsutil.MatchedWorkloadSpreadSubsetAnnotations] = string(injectWSBytes)
+
+		// Check annotations
+		actualAnnotations := updatedPod.GetAnnotations()
+		for k, v := range expectedAnnotations {
+			actualValue, ok := actualAnnotations[k]
+			if !ok {
+				t.Errorf("Missing annotation %s (expected: %s)", k, v)
+			} else if actualValue != v {
+				t.Errorf("Annotation mismatch for %s: expected %s, got %s", k, v, actualValue)
+			}
+		}
+
+		// Check labels
+		actualLabels := updatedPod.GetLabels()
+		for k, v := range expectedLabels {
+			actualValue, ok := actualLabels[k]
+			if !ok {
+				t.Errorf("Missing label %s (expected: %s)", k, v)
+			} else if actualValue != v {
+				t.Errorf("Label mismatch for %s: expected %s, got %s", k, v, actualValue)
+			}
+		}
+	})
+}
+
+func FuzzPodPreferredScore(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		cf := fuzz.NewConsumer(data)
+
+		pod := &corev1.Pod{}
+		if err := cf.GenerateStruct(pod); err != nil {
+			return
+		}
+
+		subset := &appsv1alpha1.WorkloadSpreadSubset{}
+		if err := cf.GenerateStruct(subset); err != nil {
+			return
+		}
+
+		if err := fuzzutils.GenerateWorkloadSpreadSubsetPatch(cf, subset); err != nil {
+			return
+		}
+
+		_ = podPreferredScore(subset, pod)
+	})
+}
